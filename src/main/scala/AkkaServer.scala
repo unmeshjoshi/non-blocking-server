b39300595077daafbbd12108826023ef681bb005
Saloni Vithalani <skvithalani@gmail.com>
2018-03-25 22:12:11 +0530

    Introducing nonblocking delay in server and adding it's result

--- src/main/scala/AkkaServer.scala
+++ src/main/scala/AkkaServer.scala
@@ -1,12 +1,13 @@
-import java.io.{File, FileOutputStream}
-
-import akka.actor.ActorSystem
+import akka.actor.{ActorSystem, Cancellable}
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
-import akka.stream.ActorMaterializer
 import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{Keep, Sink, Source}
 
 import scala.concurrent.ExecutionContextExecutor
+import scala.concurrent.duration.DurationInt
 import scala.io.StdIn
 
 object AkkaServer extends App {
@@ -16,36 +17,22 @@ object AkkaServer extends App {
   implicit val executionContext: ExecutionContextExecutor = system.dispatcher
 
   var counter = 0
-  val route = get {
+
+  val route: Route = get {
     path("") {
       get {
         complete({
-
-          val file = new File("/Users/salonivithalani/Desktop/tw_tmt/extra/non-blocking-server/src/main/scala/test1.txt")
-          file.createNewFile
-          val os = new FileOutputStream(file)
-          var i = 0
-          while ( {
-            i < 900000
-          }) {
-            os.write(i)
-
-            {
-              i += 1; i - 1
-            }
+          val source: Source[HttpEntity.Strict, Cancellable] = Source.tick(100.millis, 10.millis, ()).map { _ ⇒
+              counter = counter + 1
+              HttpEntity(ContentTypes.`text/plain(UTF-8)`, s"request$counter")
           }
-          os.flush()
-          os.close()
-
-          counter = counter + 1
-          HttpEntity(ContentTypes.`text/plain(UTF-8)`, s"request$counter")
+          source.toMat(Sink.head)(Keep.right).run()
         })
       }
     }
-
   }
 
-  val bindingFuture = Http().bindAndHandle(route, "localhost", 8081)
+  val bindingFuture = Http().bindAndHandle(route,"localhost", 8082)
 
   println(s"Server online at http://localhost:8081/\nPress RETURN to stop...")
   StdIn.readLine()
